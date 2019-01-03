6e5d051bc4d8108743a078544d87c5a6fe1705bd
Danny Wilson <dannywilson32@gmail.com>
2019-01-03 10:44:34 +0000

    added some path stuff

--- lisp/init-exec-path.el
+++ lisp/init-exec-path.el
@@ -1,7 +1,8 @@
 (require-package 'exec-path-from-shell)
 
+(setq exec-path-from-shell-debug t)
 (after-load 'exec-path-from-shell
-  (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE"))
+  (dolist (var '("PATH" "SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE"))
     (add-to-list 'exec-path-from-shell-variables var)))
 
 

75ba2543eea0c66dad8225ba12f1062ec9ad931b
Danny Wilson <dannywilson32@gmail.com>
2019-11-20 15:24:57 +0000

    added shell stuff and remove python stuff

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -13,41 +13,13 @@
 (global-hl-line-mode t)
 (set-face-background hl-line-face "color-236")
 
-;; Search
-(require-package 'elpy)
-
-;; ag
-(package-initialize)
-(require-package 'elpy)
-(elpy-enable)
-
-(setq python-shell-interpreter "ipython"
-      python-shell-interpreter-args "-i --simple-prompt")
-
 
 ;; font size
 (set-face-attribute 'default nil :height 140)
 
-
-;; alt as meta
-(setq mac-option-key-is-meta t)
-(setq mac-command-key-is-meta nil)
-(setq mac-command-modifier nil)
-(setq mac-option-modifier 'meta)
-
 ;; Allow hash to be entered
 (define-key key-translation-map (kbd "M-3") (kbd "#"))
 
-;; sets elpy to use pytest-runner
-(setq elpy-test-runner 'elpy-test-pytest-runner)
-
-;; remap M-. after elpy
-(define-key evil-normal-state-map "\M-." 'elpy-goto-definition)
-(define-key evil-normal-state-map "\M->" 'elpy-goto-definition-other-window)
-
-
-;; 80 width stuff
-(setq-default fill-column 80)
 
 ;; utf8 stuff
 (prefer-coding-system 'utf-8)
@@ -63,5 +35,50 @@
 
 ;; frame size
 (when window-system (set-frame-size (selected-frame) 172 60))
+
+;; shell stuff
+
+(defun preamble-regexp-alternatives (regexps)
+  "Return the alternation of a list of regexps."
+  (mapconcat (lambda (regexp)
+               (concat "\\(?:" regexp "\\)"))
+             regexps "\\|"))
+
+(defvar non-sgr-control-sequence-regexp nil
+  "Regexp that matches non-SGR control sequences.")
+
+(defun regexp-alternatives (regexps)
+  (mapconcat (lambda (regexp) (concat “\\(” regexp “\\)”)) regexps “\\|”))
+
+(setq non-sgr-control-sequence-regexp
+(regexp-alternatives
+ '(;; icon name escape sequences
+   "\033\\][0-2];.*?\007"
+   ;; non-SGR CSI escape sequences
+   "\033\\[\\??[0-9;]*[^0-9;m]"
+   ;; noop
+   "\012\033\\[2K\033\\[1F"
+   )))
+
+(defun filter-non-sgr-control-sequences-in-region (begin end)
+  (save-excursion
+    (goto-char begin)
+    (while (re-search-forward
+            non-sgr-control-sequence-regexp end t)
+      (replace-match ""))))
+
+(defun filter-non-sgr-control-sequences-in-output (ignored)
+  (let ((start-marker
+         (or comint-last-output-start
+             (point-min-marker)))
+        (end-marker
+         (process-mark
+          (get-buffer-process (current-buffer)))))
+    (filter-non-sgr-control-sequences-in-region
+     start-marker
+     end-marker)))
+
+(add-hook 'comint-output-filter-functions
+          'filter-non-sgr-control-sequences-in-output)
 
 (provide 'init-local)
